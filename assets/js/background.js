b09876c840f4d867cb6b158232b3a88743edba7d
drexa1 <drexa1@hotmail.com>
2026-02-18 20:38:25 +0100

    Refactor

--- assets/js/background.js
+++ assets/js/background.js
@@ -2,13 +2,7 @@ jobboards = ["linkedin", "jobgether", "justjoin", "web3career"]
 
 chrome.runtime.onMessage.addListener((msg, sender) => {
     if (jobboards.includes(msg.jobboard) && sender.tab?.id) {
-        if (msg.loaded) {
-            chrome.action.disable(sender.tab.id);
-            chrome.action.setBadgeText({ tabId: sender.tab.id, text: "ON" });
-            // chrome.action.setBadgeBackgroundColor({ tabId: sender.tab.id, color: "green" });
-        } else {
-            // chrome.action.enable(sender.tab.id);
-            // chrome.action.setPopup({ tabId: sender.tab.id, popup: "popup.html" });
-        }
+        chrome.action.disable(sender.tab.id);
+        chrome.action.setBadgeText({ tabId: sender.tab.id, text: "ON" });
     }
 });
